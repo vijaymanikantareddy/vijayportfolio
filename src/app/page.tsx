d9c3432401ee0b9576e8d40b97d021992ad3f1b2
vijaymanikantareddy <svmreddy7799@gmail.com>
2024-07-07 18:07:17 +0530

    Macbook Github profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
 import HomePage from "@/components/HomePage";
 import Image from "next/image";
+import { Macbook } from "@/components/MacBook";
 
 export default function Home() {
   return (
     <>
-      <main className="min-h-screen antialiased text-white bg-black/[0.96] bg-grid-white/[0.02]">
-        <div className="text-[1rem] text-center">&nbsp;</div>
+      <main
+        className="min-h-screen antialiased text-white dark:bg-grid-white/[0.08]  bg-grid-white/[0.02]"
+        id="home"
+      >
+        {/* Intro */}
         <HomePage />
+
+        {/* macbook */}
+        <Macbook />
+
+        {/* <HomePage /> */}
       </main>
     </>
   );
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -17,14 +17,14 @@ const HomePage = () => {
       />
       <div className="p-4">
         <div className="mb-6">
-          <span className="mr-4 bg-black text-white border border-[#413d3d] hover:bg-[#7B93FE] text-[14px] hover:text-black hover:duration-300 ease-in transition-all p-1 px-2 rounded-lg">
+          <span className="mr-4 bg-black text-white border border-[#413d3d] hover:bg-[#7B93FE] text-[14px] hover:text-black hover:duration-300 ease-in transition-all p-1 px-2 rounded-lg cursor-default">
             Next.js
           </span>
 
-          <span className="mr-4 bg-black text-white border border-[#413d3d] hover:bg-[#7B93FE] text-[14px] hover:text-black hover:duration-300 ease-in transition-all p-1 px-2 rounded-lg">
+          <span className="mr-4 bg-black text-white border border-[#413d3d] hover:bg-[#7B93FE] text-[14px] hover:text-black hover:duration-300 ease-in transition-all p-1 px-2 rounded-lg cursor-default">
             TailwindCSS
           </span>
-          <span className="bg-black text-white border border-[#413d3d] hover:bg-[#7B93FE] text-[14px] hover:text-black hover:duration-300 ease-in transition-all p-1 px-2 rounded-lg">
+          <span className="bg-black text-white border border-[#413d3d] hover:bg-[#7B93FE] text-[14px] hover:text-black hover:duration-300 ease-in transition-all p-1 px-2 rounded-lg cursor-default">
             Typescript
           </span>
         </div>
@@ -38,20 +38,20 @@ const HomePage = () => {
               Vijay!
             </span>
           </h1>
-          <p className="mt-4 max-w-lg tracking-tight text-muted-foreground 2xl:text-xl">
+          <p className="mt-4 max-w-lg text-[#91a0b3] tracking-tight text-muted-foreground 2xl:text-xl">
             A MERN Stack Web Developer | Knight Badge Holder in Leetcode
           </p>
         </div>
         <div className="mt-10">
           <Link
             href="#contact"
-            className="bg-[#7B93FE] text-black py-3 px-6 text-[16px] mr-2 rounded-lg font-bold"
+            className="bg-[#7B93FE] text-black hover:bg-[#899cf2] hover:duration-100 ease-in transition-all py-3 px-6 text-[16px] mr-2 rounded-lg font-bold"
           >
             Get in touch
           </Link>
           <Link
             href="#about"
-            className="bg-black border border-[#413d3d] py-3 px-6 text-[16px] mr-2 rounded-lg font-bold"
+            className="bg-black border border-[#413d3d] hover:bg-[#161616] hover:duration-100 ease-in transition-all py-3 px-6 text-[16px] mr-2 rounded-lg font-bold"
           >
             Know More
           </Link>
@@ -60,7 +60,7 @@ const HomePage = () => {
       <div
         data-scroll
         data-scroll-speed="-.01"
-        className="mt-14 xl:mt-0 canvas-container border border-[#413d3d] rounded-3xl h-[515px] xl:w-[690px] xl:min-w-[690px] overflow-hidden"
+        className="mt-14 mx-4 xl:mt-0 canvas-container border border-[#413d3d] rounded-3xl h-[515px] xl:w-[690px] xl:min-w-[690px] overflow-hidden cursor-grabbing"
       >
         <Suspense fallback={<span>Loading...</span>}>
           <Spline scene="/assets/scene.splinecode" />
